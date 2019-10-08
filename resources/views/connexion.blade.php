2374c3e63c733378f20105621dd69943b181dcd0
Rutherford05 <nguingandjifils@gmail.com>
2019-10-08 13:06:46 +0000

    Faire

--- resources/views/connexion.blade.php
+++ resources/views/connexion.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-            <div class="card-header bg-dark text-white">{{ __('Se Connecter') }}</div>
+            <div class="card-header bg-dark text-white">{{ __('INSCRIPTION') }}</div>
                 <div class="panel-body">
                     <form class="form-horizontal" method="POST" action="{{ route('register') }}">
                         {{ csrf_field() }}
